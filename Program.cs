4310bd0626c03158695d52d342651ebbcdcaed75
Dax272 <49327226+Dax272@users.noreply.github.com>
2021-08-15 21:49:15 -0700

    uploading mainframe

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShitConsoleThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many students are in your class: ");
+            
+            var arrayInput = Convert.ToInt32(Console.ReadLine());
+            string[] names = new string[arrayInput];
+            
+            Console.WriteLine("Please input the names of the students: ");
+            
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("--------------------");
+            Array.Sort(names);
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
